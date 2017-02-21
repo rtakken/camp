219504f6cd28a6fee7651c4570c403dbf4f6cbe6
Reid Takken <takkenr@gmail.com>
2017-02-20 21:20:35 -0500

    small changes

--- server.py
+++ server.py
@@ -19,38 +19,42 @@ import tornado.web
 import tornado.websocket
 from tornado.ioloop import PeriodicCallback
 
-# Hashed password for comparison and a cookie for login cache
-ROOT = os.path.normpath(os.path.dirname(__file__))
-with open(os.path.join(ROOT, "password.txt")) as in_file:
-    PASSWORD = in_file.read().strip()
-COOKIE_NAME = "camp"
+
+# class IndexHandler(tornado.web.RequestHandler):
+
+#     def get(self):
+#         if args.require_login and not self.get_secure_cookie(COOKIE_NAME):
+#             self.redirect("/login")
+#         else:
+#             self.render("index.html", port=args.port)
 
 
-class IndexHandler(tornado.web.RequestHandler):
+# class LoginHandler(tornado.web.RequestHandler):
 
-    def get(self):
-        if args.require_login and not self.get_secure_cookie(COOKIE_NAME):
-            self.redirect("/login")
-        else:
-            self.render("index.html", port=args.port)
+#     def get(self):
+#         self.render("login.html")
 
-
-class LoginHandler(tornado.web.RequestHandler):
-
-    def get(self):
-        self.render("login.html")
-
-    def post(self):
-        password = self.get_argument("password", "")
-        if hashlib.sha512(password).hexdigest() == PASSWORD:
-            self.set_secure_cookie(COOKIE_NAME, str(time.time()))
-            self.redirect("/")
-        else:
-            time.sleep(1)
-            self.redirect(u"/login?error")
+#     def post(self):
+#         password = self.get_argument("password", "")
+#         if hashlib.sha512(password).hexdigest() == PASSWORD:
+#             self.set_secure_cookie(COOKIE_NAME, str(time.time()))
+#             self.redirect("/")
+#         else:
+#             time.sleep(1)
+#             self.redirect(u"/login?error")
 
 
 class WebSocket(tornado.websocket.WebSocketHandler):
+    
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print "connection opened"
+        self.write_message("connection opened")
+
+    def on_close(self):
+        print "connection closed"
 
     def on_message(self, message):
         """Evaluates the function pointed to by json-rpc."""
@@ -113,12 +117,8 @@ if args.resolution in resolutions:
 else:
     raise Exception("%s not in resolution options." % args.resolution)
 
-handlers = [(r"/", IndexHandler), (r"/login", LoginHandler),
-            (r"/websocket", WebSocket),
-            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': ROOT})]
-application = tornado.web.Application(handlers, cookie_secret=PASSWORD)
+handlers = [(r"/websocket", WebSocket)]
+application = tornado.web.Application(handlers)
 application.listen(args.port)
-
-webbrowser.open("http://localhost:%d/" % args.port, new=2)
 
 tornado.ioloop.IOLoop.instance().start()
